45e1af4f8f97f6a3289146efa29d557165bb2084
c17hawke <sunny.c17hawke@gmail.com>
2022-09-01 18:09:19 +0530

    st 02 | entiry defined

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -6,3 +6,14 @@ DataIngestionConfig = namedtuple("DataIngestionConfig", [
     "local_data_file",
     "unzip_dir"
 ])
+
+PrepareBaseModelConfig = namedtuple("PrepareBaseModelConfig",[
+    "root_dir",
+    "base_model_filepath",
+    "updated_base_model_path",
+    "param_image_size",
+    "param_classes",
+    "param_learning_rate",
+    "param_include_top",
+    "param_weights"
+])
