0a5f4f32f2f8602ccdd25233671d40c70c1c922b
c17hawke <sunny.c17hawke@gmail.com>
2022-09-01 21:19:12 +0530

    st 03 | train pipeline update

---
+++ src/deepClassifier/pipeline/stage_03_training.py
@@ -0,0 +1,31 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareCallbacks, Training
+from deepClassifier import logger
+
+STAGE_NAME = 'Prepare Base Model'
+
+def main():
+    config  =  ConfigurationManager()
+
+    callback_config = config.get_callbacks_config()
+    callbacks = PrepareCallbacks(callback_config)
+    callback_list = callbacks.get_tb_ckpt_callbacks()
+
+    training_config = config.get_training_config()
+    training = Training(training_config)
+    training.get_base_model()
+    training.train_valid_generator()  
+    training.train(
+        callback_list=callback_list
+    )
+
+
+if __name__ == "__main__":
+    try:
+        logger.info("\n********************")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        main()
+        logger.info(f"\n>>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
