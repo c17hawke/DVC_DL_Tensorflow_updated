ab6b1e9cbc21c54fa402a2edffcb2b0e51e47cc1
c17hawke <sunny.c17hawke@gmail.com>
2022-09-01 16:32:20 +0530

    update with verbose default True

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -35,7 +35,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
 
 @ensure_annotations
-def create_directories(path_to_directories: list, ignore_log=False):
+def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
     Args:
@@ -44,7 +44,7 @@ def create_directories(path_to_directories: list, ignore_log=False):
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
-        if not ignore_log:
+        if verbose:
             logger.info(f"created directory at: {path}")
 
 @ensure_annotations
