56872567b21a64478b8f4693840e9ad89b7ec464
c17hawke <sunny.c17hawke@gmail.com>
2022-09-01 16:04:19 +0530

    log filepathdefined

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -5,11 +5,13 @@ import inspect
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
+log_filepath = os.path.join(log_dir, 'running_logs.log')
 os.makedirs(log_dir, exist_ok=True)
+
 logging.basicConfig(
     level=logging.INFO, format=logging_str,
     handlers=[
-        logging.FileHandler(os.path.join(log_dir, 'running_logs.log')),#, mode="a"),
+        logging.FileHandler(log_filepath),#, mode="a"),
         logging.StreamHandler(sys.stdout)
     ])
 
