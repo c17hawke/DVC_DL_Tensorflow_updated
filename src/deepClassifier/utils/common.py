0e2754ebdf9bdde91369e6bf304d5c9c5f478bc6
c17hawke <sunny.c17hawke@gmail.com>
2022-09-01 16:03:41 +0530

    update docstrings and data type strictness

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -7,9 +7,11 @@ import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
+from pathlib import Path
+from typing import Any
 
 @ensure_annotations
-def read_yaml(path_to_yaml: str) -> ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
     Args:
@@ -33,33 +35,81 @@ def read_yaml(path_to_yaml: str) -> ConfigBox:
         raise e
 
 @ensure_annotations
-def create_directories(path_to_directories: list) -> None:
+def create_directories(path_to_directories: list, ignore_log=False):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
-        logger.info(f"created directory at: {path}")
+        if not ignore_log:
+            logger.info(f"created directory at: {path}")
 
 @ensure_annotations
-def save_json(path: str, data: dict) -> None:
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
 
 @ensure_annotations
-def load_json(path: str) -> None:
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
     with open(path) as f:
         content = json.load(f)
 
     logger.info(f"json file loaded succesfully from: {path}")
-    return content
+    return ConfigBox(content)
 
 @ensure_annotations
-def save_bin(data, path: str) -> None:
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
     joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
 
 @ensure_annotations
-def load_bin(path: str):
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
     return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
